f44e544ef0149ed411846f23173e6b7d56f5364a
simorai <109289730+simorai@users.noreply.github.com>
2025-06-12 11:12:31 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppLanches
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Services/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLanches.Services
+{
+    public class ApiResponse<T>
+    {
+        public T? Data { get; set; }
+        public string? ErrorMessage { get; set; }
+        public bool HasError => !string.IsNullOrEmpty(ErrorMessage);
+    }
+}
